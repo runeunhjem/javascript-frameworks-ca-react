116a9e6d04477e71b111c2deb389e060e70f34a5
runeunhjem <runeunhjem@gmail.com>
2024-03-06 11:56:33 +0100

    Adds search results styled components

--- src/pages/SearchResultsPage/index.jsx
+++ src/pages/SearchResultsPage/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useMemo } from "react";
 import { useProducts } from "../../contexts/ProductContext/useProducts";
 import ProductCard from "../../components/ProductCard";
+import * as S from "./index.styled"; // Ensure this path matches where your styled components are defined
 
 function SearchResultsPage() {
   useEffect(() => {
@@ -19,11 +20,17 @@ function SearchResultsPage() {
   }, [products, searchTerm]);
 
   return (
-    <div className="product-cards">
-      {filteredProducts.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+    <>
+      <S.Heading>Search Results</S.Heading>
+      <S.SearchResultInfo>
+        Displaying {filteredProducts.length} results for &quot;{searchTerm}&quot;
+      </S.SearchResultInfo>
+      <S.ProductCards>
+        {filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </S.ProductCards>
+    </>
   );
 }
 
